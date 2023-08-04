c9552cd4023a183b66506b2bea58e5e015940fb1
Binzen <129286646+BrunoZENARI@users.noreply.github.com>
2023-08-04 16:31:59 +0200

    build table_page

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,7 +20,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Email:',
+                'attr' => ['class' => 'form-control', 'required' => 'required']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -31,7 +36,9 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                            'class' => 'form-control', 'required' => 'required'],
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -44,8 +51,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('lastname', TextType::class)
-            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class, [
+                'label' => 'Lastname:',
+                'attr' => ['class' => 'form-control', 'required' => 'required']
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Firstame:',
+                'attr' => ['class' => 'form-control', 'required' => 'required']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => ['class' => 'btn btn-primary']
+            ])
         ;
     }
 
