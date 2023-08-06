376e5df1b9a0b991575ba6fb5b75949db763da97
Binzen <129286646+BrunoZENARI@users.noreply.github.com>
2023-08-06 17:09:38 +0200

    Build calendar index_page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\TachesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/index', name: 'app_index')]
-    public function index(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(TachesRepository $tachesRepository): Response
     {
         return $this->render('index.html.twig', [
             'controller_name' => 'IndexController',
